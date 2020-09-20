a1d5edb0b73ea525d18208564bb065364a5ef0db
Nick Avina <avinanick@gmail.com>
2020-09-20 12:05:23 -0700

    Added an email details view for viewing an email, nothing in it just yet

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -15,6 +15,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-details-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -61,6 +62,11 @@ function create_email_list_element(email_json) {
 
 function load_email(email_id) {
 
+  // Show email details view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-details-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
 }
 
 function load_mailbox(mailbox) {
@@ -82,6 +88,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-details-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
